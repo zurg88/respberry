073d76a5d02e5966fa18f6f5bee76cc0003a4d68
Sergey Gnatenko <gnatenko@gmail.com>
2020-10-27 23:25:43 +0300

    create mega menu

--- src/modules/animations.js
+++ src/modules/animations.js
@@ -2,13 +2,13 @@ const projectAnimations = () => {
 	const	accountListItems = document.querySelectorAll('.account-list-link'),
 		 userMenuContainer = document.querySelector('.user-menu-container'),
 		 dresses = document.querySelector('.dresses'),
-		 megaMenu = document.querySelector('.mega-menu'),
 		 dropdownLinks = document.querySelectorAll('.dropdown-link'),
+		 megaMenu = document.querySelector('.mega-menu'),
 		 megaMenuBlock = document.querySelectorAll('.mega-menu-block');
 
 
 	const userMenuAnimation = (container, items, delay) => {
-		container.addEventListener('mouseover', () => {
+		container.addEventListener('mouseenter', () => {
 			let count = 0;
 			
 			const intervalId = setInterval(() => {
@@ -18,18 +18,18 @@ const projectAnimations = () => {
 				items[count].style.opacity = '1';
 					count++;
 			}, delay);
-		});
+		}, false);
 		
-		container.addEventListener('mouseout', () => {
+		container.addEventListener('mouseleave', () => {
 			items.forEach(item => {
 				item.style.opacity = '0';
 			});
-		});
+		}, false);
 	};
 
 	userMenuAnimation(userMenuContainer, accountListItems, 25);
 	userMenuAnimation(dresses, dropdownLinks, 25);
-	// userMenuAnimation(megaMenu, megaMenuBlock, 100);
+	userMenuAnimation(megaMenu, megaMenuBlock, 100);
 
 	
 };
