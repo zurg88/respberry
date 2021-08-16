429850eab58a9a2db11bd83382b224bdcf427216
Sergey Gnatenko <gnatenko@gmail.com>
2021-08-16 22:21:53 +0300

    create shop filter module

--- src/modules/shop-filters.js
+++ src/modules/shop-filters.js
@@ -1,7 +1,138 @@
+'use strict';
+
 const filters = ()=> {
-	const priceFilterBlock = document.querySelector('.price-filter-block');
+	const priceFilterBlock = document.querySelector('.price-filter-block'),
+		productList = document.querySelector('.product-list'),
+		tableViewBtn = document.getElementById('table-view'),
+		sortingSelect = document.getElementById('sorting-select'),
+		listViewBtn = document.getElementById('list-view'),
+		viewBtnsArr = document.querySelectorAll('.grid-view'),
+		productCategoriesLink = document.querySelectorAll('.product-categories-link'),
+		productListItems = document.querySelectorAll('.product-list-item'),
+		colorFilterItemLink = document.querySelectorAll('.color-filter-item-link'),
+		productTagsItem = document.querySelectorAll('.product-tags-item'),
+		rangeSlider = document.querySelector('.range-slider'),
+		showingElems = document.getElementById('showing-elems'),
+		resultsElems = document.getElementById('results-elems');
+	
 
+	const showQuantityVisibleProducts = () => {
+		const products = productList.querySelectorAll('.product-list-item');
 
+		showingElems.textContent = products.length;
+	};
+
+	const showQuantityProducts = () => {
+		resultsElems.textContent = productListItems.length;
+	};
+
+	showQuantityProducts();
+	showQuantityVisibleProducts();
+
+	viewBtnsArr.forEach(item => {
+		item.addEventListener('click', (e)=>{
+			const target = e.target;
+			viewBtnsArr.forEach(item => {
+				item.classList.remove('active-view');
+			});
+			target.closest('.grid-view').classList.add('active-view');
+			if(target.closest('#table-view')) {
+				productList.classList.add('product-list-table-view');
+			} else {
+				productList.classList.remove('product-list-table-view');
+			}
+		});
+	});
+
+	const findCategoryItems = (categoryName)=> {
+		const productArr = [];
+		productListItems.forEach(item => {
+			if(item.classList.contains(categoryName)) {
+				productArr.push(item);
+			}
+		});
+		productList.innerHTML = '';
+		productArr.forEach(item => {
+			productList.append(item);
+		});
+		showQuantityVisibleProducts();
+	};
+
+	const categoriesTagsFilter = (filterСriteria)=> {
+		filterСriteria.forEach(item => {
+			
+			item.addEventListener('click', (event) => {
+				event.preventDefault();
+				const itemContent = item.textContent;
+				console.log(itemContent.toLowerCase());
+				findCategoryItems(itemContent.toLowerCase());
+			});
+		});
+
+		showQuantityVisibleProducts();
+	};
+
+	categoriesTagsFilter(productCategoriesLink);
+	categoriesTagsFilter(colorFilterItemLink);
+	categoriesTagsFilter(productTagsItem);
+
+	const productFilterPrice = () => {
+		const products = productList.querySelectorAll('.product-list-item'),
+			productsPriceArr = [];
+		
+		products.forEach(item => {
+			productsPriceArr.push(item.querySelector('.price'));
+		});
+
+		function compareNumbers(a, b) {
+			return a.textContent - b.textContent;
+		  }
+
+		productsPriceArr.sort(compareNumbers);
+	
+		productList.innerHTML = '';
+		productsPriceArr.forEach(item => {
+			productList.append(item.closest('.product-list-item'));
+		});
+
+		showQuantityVisibleProducts();
+	};
+
+	sortingSelect.addEventListener('change', ()=> {
+		if(sortingSelect.value === 'price') {
+			productFilterPrice();
+		}
+		if(sortingSelect.value === 'default sorting') {
+			productListItems.forEach(item => {
+				productList.append(item);
+			});
+		}
+	});
+
+	rangeSlider.noUiSlider.on('change', function(){
+		const sliderValue = rangeSlider.noUiSlider.get(),
+			products = productList.querySelectorAll('.product-list-item');
+		
+		let count = products.length;
+		
+			products.forEach(item=> {
+				if(item.querySelector('.price').textContent < +sliderValue[0]) {
+					item.style.display = 'none';
+				} else if(item.querySelector('.price').textContent > +sliderValue[1]) {
+					item.style.display = 'none';
+				} else {
+					item.style.display = 'flex';
+				}
+			});
+
+			products.forEach(item=> {
+				if(item.style.display === 'none') {
+					count = count - 1;
+				}
+			});
+
+			showingElems.textContent = count;
+	});
 };
 
 export default filters;
